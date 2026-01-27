ad8d5f1c2dbe57979b6b632496f156d102f6b8c8
ankitkhatri1 <118861040+ankitkhatri1@users.noreply.github.com>
2026-01-26 18:49:59 -0600

    hi

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
